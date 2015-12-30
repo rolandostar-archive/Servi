6694f1d68a24afe5009f2083857169b0b0f91f31
Rolando Romero <rolandostar.pr@gmail.com>
2015-12-30 14:16:45 -0600

    Implementado Notificacion por email

--- Scripts/agregar.php
+++ Scripts/agregar.php
@@ -1,7 +1,38 @@
 <?php 
+include_once ('../cpanel/dbconnect.php');
+
+switch ($_POST['lang']) {
+    case "es": 
+        $msg_pre = "Salon ";
+        $msg_pos = " ya esta disponible";
+    break;
+    case "en":
+        $msg_pre = "Room ";
+        $msg_pos = " now available";
+    break;
+    case "jp":
+        $msg_pre = "利用可能になりまし教室";
+        $msg_pos = "";
+    break;
+    case "it":
+        $msg_pre = "Aula ";
+        $msg_pos = " ora disponibile";
+    break;
+    case "fr":
+        $msg_pre = "Salle de classe ";
+        $msg_pos = " maintenant disponible";
+    break;
+}
+// ENVIARA EMAIL EN 1 MINUTO
+
 
 $fichero='output.txt';
 $actual = file_get_contents($fichero);
 $actual .= print_r($_POST,true);
+$query = "INSERT INTO notify (id,lang,correo,msg,tiempo,enviado) VALUES (NULL,'".$_POST['lang']."','".$_POST['email']."','".$msg_pre.$_POST['Salon'].$msg_pos."',NOW(),0)";
+//$actual .= $query;
+mysql_query($query);
+
+
 file_put_contents($fichero, $actual);
 ?>
